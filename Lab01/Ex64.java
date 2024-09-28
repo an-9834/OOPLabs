66b6869ff5d8d4601d70b5f17a70d50aa0abd309
Chu An <146003238+an-9834@users.noreply.github.com>
2024-09-28 19:02:37 +0700

    Delete Lab01 directory

--- Lab01/Ex64.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Days{
-    public static void main(String[] args) {
-        Scanner infors = new Scanner(System.in);
-        int year = infors.nextInt();
-        
-    }
-}
