939bb990325328e8391aec022a3bed8e04b9bbd5
Chu An <146003238+an-9834@users.noreply.github.com>
2024-09-28 18:47:59 +0700

    Create Ex64.java

---
+++ Lab01/Ex64.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Days{
+    public static void main(String[] args) {
+        Scanner infors = new Scanner(System.in);
+        int year = infors.nextInt();
+        
+    }
+}
