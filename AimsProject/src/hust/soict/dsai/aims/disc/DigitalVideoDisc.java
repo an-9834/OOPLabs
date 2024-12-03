4e2592480ef10ca478d1cb560ec0f242ebbb2e4f
Chu An <146003238+an-9834@users.noreply.github.com>
2024-12-03 12:05:09 +0700

    Add files via upload

---
+++ AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
@@ -0,0 +1,107 @@
+package hust.soict.dsai.aims.disc;
+
+
+public class DigitalVideoDisc {
+	private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    private static int nbDigitalVideoDiscs = 0;
+	private int id;
+
+    public String getTitle() {
+        return title;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public int getLength() {
+        return length;
+    }
+    public float getCost() {
+        return cost;
+    }
+    public int getID(){
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public DigitalVideoDisc(String title) {
+    	super();
+        this.title = title;
+        nbDigitalVideoDiscs += 1;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+    	super();
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        nbDigitalVideoDiscs += 1;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+    	super();
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+        nbDigitalVideoDiscs += 1;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+    	super();
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+        nbDigitalVideoDiscs += 1;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public String toString(){
+        String linePrint;
+        if (length != 0){
+            linePrint = "DVD - " + title + " - " + category + " - " + length + ": " + cost + "$";
+        } else {
+            linePrint = "DVD - " + title + " - " + category + " - " + cost + "$";
+        }
+        return linePrint;
+    }
+
+    public boolean isMatch(String title) {
+		String [] keyword = title.toLowerCase().split(" ");
+		String tmp = this.getTitle().toLowerCase();
+		for(String key : keyword) {
+			if(tmp.contains(key)) return true;
+		}
+		return false;
+	}
+
+}
